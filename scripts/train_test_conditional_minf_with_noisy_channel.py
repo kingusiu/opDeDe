fc740edfdc54cbaea41a549e7601ec31de78dc6d
kingusiu <kinga.wozniak@gmail.com>
2024-08-07 22:33:36 +0200

    adding datasets and dataloader for conditional test

--- scripts/train_test_conditional_minf_with_noisy_channel.py
+++ scripts/train_test_conditional_minf_with_noisy_channel.py
@@ -1,17 +1,43 @@
+import torch
 from minfnet.dats import input_generator as inge
 
 
 def main():
+
+    #****************************************#
+    #    runtime params
+    #****************************************#
+
+    batch_size = 256
     
     #****************************************#
     #               load data 
     #****************************************#
+    N_per_theta = int(1e3)
+    B_N = 1
 
-    A_train, B_train, A_test, B_test = inge.read_inputs_from_df(df, b_label=params, train_test_split=0.5)
+    A_train_all = []
+    B_train_all = []
+    A_test_all = []
+    B_test_all = []
+    theta_train_all = []
+    theta_test_all = []
 
-    # import ipdb; ipdb.set_trace()
+    for theta in [0.1, 0.5, 1.0, 1.5, 2.0]:
+        A_train, B_train, A_test, B_test, theta_train, theta_test = inge.generate_noisy_channel_samples(N=N_per_theta, noise_std_nominal=theta, train_test_split=0.7)
+        A_train_all.append(A_train)
+        B_train_all.append(B_train)
+        A_test_all.append(A_test)
+        B_test_all.append(B_test)
+        theta_train_all.append(theta_train)
+        theta_test_all.append(theta_test)
+# import ipdb; ipdb.set_trace()
     
-    B_N = B_train.size(1)
+    dataset_train = inge.MinfDataset(A_var=A_train_all, B_var=B_train_all, thetas=theta_train_all)
+    dataset_test = inge.MinfDataset(A_var=A_test_all, B_var=B_test_all, thetas=theta_test_all)
+
+    train_dataloader = torch.utils.data.DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+    test_dataloader = torch.utils.data.DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
     
     #****************************************#
     #               build model 
